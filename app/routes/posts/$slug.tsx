e2b783b002da90cbda611aa0bf5220429eaa0626
hendraaagil <hendraaagil@gmail.com>
2022-03-20 00:34:01 +0700

    chore: init dev blog using remix

---
+++ app/routes/posts/$slug.tsx
@@ -0,0 +1,15 @@
+import { json, useLoaderData } from 'remix';
+import type { LoaderFunction } from 'remix';
+import invariant from 'tiny-invariant';
+
+import { getPost } from '~/post';
+
+export const loader: LoaderFunction = async ({ params }) => {
+  invariant(params.slug, 'expected params.slug');
+  return json(await getPost(params.slug));
+};
+
+export default function PostSlug() {
+  const post = useLoaderData();
+  return <main dangerouslySetInnerHTML={{ __html: post.html }} />;
+}
